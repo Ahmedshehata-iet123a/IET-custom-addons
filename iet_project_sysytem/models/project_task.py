841f076d8d6bcd7a8ef13c2d39b638608f89b6ce
ahmed-shehata <ahmed.shehata@intelligent-experts.com>
2025-08-10 17:09:15 +0300

    up

--- iet_project_sysytem/models/project_task.py
+++ iet_project_sysytem/models/project_task.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class ProjectTask(models.Model):
@@ -7,3 +8,24 @@ class ProjectTask(models.Model):
     team_name = fields.Char(string="Team Name", required=True)
     date_start = fields.Datetime(string="Start Date")
     end_date = fields.Datetime(string="End Date")
+
+    def write(self, vals):
+        if 'stage_id' in vals:
+            for task in self:
+                stage = self.env['project.task.type'].browse(vals['stage_id'])
+
+                if stage.required_deadline:
+                    date_deadline = vals.get('date_deadline', task.date_deadline)
+                    if not date_deadline:
+                        raise ValidationError(
+                            _("You must fill in 'Deadline Date' before changing stage.")
+                        )
+
+                if stage.required_allocated:
+                    allocated_hours = vals.get('allocated_hours', task.allocated_hours)
+                    if not allocated_hours:
+                        raise ValidationError(
+                            _("You must fill in 'Allocated Hours' before changing stage.")
+                        )
+
+        return super().write(vals)
--- iet_project_sysytem/models/project_task_type.py
+++ iet_project_sysytem/models/project_task_type.py
@@ -4,5 +4,6 @@ from odoo import models, fields
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
 
-
     generate_tasks = fields.Boolean(string="Generate Tasks")
+    required_deadline = fields.Boolean(string="Required Deadline")
+    required_allocated = fields.Boolean(string="Required Allocated")
